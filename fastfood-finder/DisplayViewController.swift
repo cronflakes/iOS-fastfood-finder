073984b880f50a0ade61a4d4783a02c7d742da4c
Matthew Cassell <matthewcassell@matthews-mbp-3.attlocal.net>
2018-10-05 17:35:48 -0500

    Initial Commit

---
+++ fastfood-finder/DisplayViewController.swift
@@ -0,0 +1,22 @@
+//
+//  DisplayViewController.swift
+//  fastfood-finder
+//
+//  Created by Matthew Cassell on 10/5/18.
+//  Copyright © 2018 Matthew Cassell. All rights reserved.
+//
+
+import UIKit
+
+class DisplayViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+  
+    @IBAction func cancelButtonTapped(_ sender: UIBarButtonItem) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    
+}
---
+++ fastfood-finder/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  fastfood-finder
+//
+//  Created by Matthew Cassell on 10/5/18.
+//  Copyright © 2018 Matthew Cassell. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    @IBAction func findFoodButtonTapped(_ sender: UIButton) {
+    }
+    
+}
+
