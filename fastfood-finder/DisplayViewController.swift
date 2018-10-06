8ab2e1b9a554f20b6ec979ade1b4f117991aacd1
Matthew Cassell <matthewcassell@matthews-mbp-3.attlocal.net>
2018-10-05 22:50:09 -0500

    find users initial location added

--- fastfood-finder/DisplayViewController.swift
+++ fastfood-finder/DisplayViewController.swift
@@ -7,13 +7,48 @@
 //
 
 import UIKit
+import MapKit
 
-class DisplayViewController: UIViewController {
 
+class DisplayViewController: UIViewController, CLLocationManagerDelegate {
+    
+    @IBOutlet weak var mapView: MKMapView!
+    
+    private var locationManager = CLLocationManager()
+    private var initialLocation: CLLocationCoordinate2D?
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.startUpdatingLocation()
+        
+        mapView.showsUserLocation = true
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        if let location = locationManager.location?.coordinate {
+            initialLocation = CLLocationCoordinate2D(latitude: location.latitude, longitude: location.longitude)
+            let region = MKCoordinateRegion(center: locationConverter(location: initialLocation!).coordinate, latitudinalMeters: 1000, longitudinalMeters: 1000)
+            self.mapView.setRegion(region, animated: true)
+        } else { print("Unable to find location") }
+    }
+   
+    
+    func locationConverter(location: CLLocationCoordinate2D) -> CLLocation  {
+        let lat: CLLocationDegrees = location.latitude
+        let long: CLLocationDegrees = location.longitude
+        
+        let convertedLocation = CLLocation(latitude: lat, longitude: long)
+        
+        return convertedLocation
     }
   
+
+    
     @IBAction func cancelButtonTapped(_ sender: UIBarButtonItem) {
         dismiss(animated: true, completion: nil)
     }
---
+++ fastfood-finder/RootClass.swift
@@ -0,0 +1,31 @@
+//
+//  RootClass.swift
+//  fastfood-finder
+//
+//  Created by Matthew Cassell on 10/5/18.
+//  Copyright © 2018 Matthew Cassell. All rights reserved.
+//
+
+import Foundation
+
+public struct RootClass: Decodable {
+    public var name: String!
+    public var address: String!
+    public var phone: String!
+    public var hours: [Hours]!
+    public var image: String!
+    public var logo: String!
+    public var latitude: String!
+    public var longitude: String!
+    
+}
+
+public struct Hours: Decodable {
+    public var monday: String!
+    public var tuesday: String!
+    public var wednesday: String!
+    public var thursday: String!
+    public var friday: String!
+    public var saturday: String!
+    public var sunday: String!
+}
